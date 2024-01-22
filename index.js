16f76d0a35f76d9d3a3f013c4ec5dbb109a27646
Victor Rojas <vicrojas_mx@hotmail.com>
2024-01-21 21:42:10 -0500

    Refactor load cards from local storage

--- index.js
+++ index.js
@@ -1,34 +1,5 @@
 import Kanban from "./kanban.js";
 
-/* const todoCards = document.querySelector(".cards.todo");
-const pendingCards = document.querySelector(".cards.pending");
-const completedCards = document.querySelector(".cards.completed");
- */ 
-function fillUpTaskBoxes(columnId) {
-    let taxBoxCardsContent = "";
-    try {
-        Kanban.getTasks(columnId).forEach(element => {
-            taxBoxCardsContent += 
-            `<form class="card">
-            <input type="text" name="task" autocomplete="off" disabled="disabled" value="${element.content}">
-            <div>
-                <span class="task-id">${element.taskId}</span>
-                <span>
-                    <button class="bi bi-pencil edit" data-id=""></button>
-                    <button class="bi bi-check-lg update hide" data-id=""></button>
-                    <button class="bi bi-trash3 delete" data-id=""></button>
-                </span>
-            </div>
-        </form>
-        `
-        });
-    }
-    catch (error) {
-        console.error(`Column number out of index --- ${error}`);
-    }
-    return taxBoxCardsContent;
-}
-
 console.log(Kanban.getAllTasks());
 console.log("Fetching the data");
 const todoCards = document.querySelector(".cards.todo");
@@ -45,17 +16,22 @@ const taskbox = [todoCards, pendingCards, completedCards];
 console.log(Kanban.getAllTasks()[0]);
 
 function addTask(task, index) {
-        taskbox[index].innerHTML += `<form class="card" draggable="true">
-             <input type="text" name="task" autocomplete="off" disabled="disabled" value="${task.content}">
-             <div>
-                <span class="task-id">${task.taskId}</span>
+    const element = document.createElement("form");
+    element.className = "card";
+    element.draggable = true;
+    element.dataset.id = task.taskId;
+    element.innerHTML = `
+        <input type="text" name="task" autocomplete="off" disabled="disabled" value="${task.content}">
+            <div>
+                <span class="task-id">#${task.taskId}</span>
                 <span>
-                   <button class="bi bi-pencil edit" data-id=""></button>
-                   <button class="bi bi-check-lg update hide" data-id=""></button>
-                   <button class="bi bi-trash3 delete" data-id=""></button>
+                   <button class="bi bi-pencil edit" data-id="${task.taskId}"></button>
+                   <button class="bi bi-check-lg update hide" data-id="${task.taskId}"></button>
+                   <button class="bi bi-trash3 delete" data-id="${task.taskId}"></button>
                 </span>
              </div>
-            </form>`
+    `;
+    taskbox[index].appendChild(element);
 }
 
 Kanban.getAllTasks().forEach((tasks, index) => {
